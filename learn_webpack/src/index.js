840b98d3bf8b6d70a92ffb3695eee7dca5c0ba0c
eric <1210741229@qq.com>
2017-11-06 12:53:48 +0800

    20171106

---
+++ learn_webpack/src/index.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import printMe from './print';
+
+
+function component(){
+	var element = document.createElement('div');
+	var btn = document.createElement('button');
+	element.innerHTML = _.join(['你好呀!','webpack'],' ');
+	btn.innerHTML = 'Clike me and check the console';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+	return element;
+}
+document.body.appendChild(component());
+
+if(module.hot){
+	module.hot.accept('./print.js',function(){
+		console.log('Accepting the updated printMe module!');
+		printMe();
+	})
+}
